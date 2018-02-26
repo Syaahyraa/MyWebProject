36c1b9a4a58afacd1f36ca32ae1e957684a16cba
Syaahyraa <syahirahnz78@gmail.com>
2018-02-26 14:01:54 +0800

    Add Number Test

---
+++ AddNumber/src/AddNumber.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class AddNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int x = 10;
+		 int y = 20 ;
+		 int sum;
+		 
+	     sum = x + y;
+	     
+	     System.out.println("Number 1= "+x);
+	     System.out.println("Number 2= "+y);
+	     System.out.println("Addition of both number = "+sum);
+	}
+
+}
